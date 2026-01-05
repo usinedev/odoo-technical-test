3809776cf49e75ec4f101ba87a5da9a9265fe350
Sacha <124590090+usinedev@users.noreply.github.com>
2026-01-05 10:33:36 +0100

    First commit - blank base

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Cta from '@/components/Cta.vue';
+import Faq from '@/components/Faq.vue';
+import FeatureSection from '@/components/FeatureSection.vue';
+import Footer from '@/components/Footer.vue';
+import Header from '@/components/Header.vue';
+import Hero from '@/components/Hero.vue';
+import Ratings from '@/components/Ratings.vue';
+</script>
+
+<template>
+    <Header />
+    <Hero />
+    <FeatureSection />
+    <Cta />
+    <Ratings />
+    <Faq />
+    <Footer />
+</template>
+
+<style scoped lang="scss">
+@use '../assets/style/main.scss' as *;
+</style>
