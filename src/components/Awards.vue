d828d644fa5d5116bfea047f27927c67419809e8
Sacha <124590090+usinedev@users.noreply.github.com>
2026-01-06 13:58:00 +0100

    faq - desktop

---
+++ src/components/Awards.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <section class="awardsSection">
+        
+    </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Awards from '@/components/Awards.vue';
 import Cta from '@/components/Cta.vue';
 import Faq from '@/components/Faq.vue';
 import FeatureSection from '@/components/FeatureSection.vue';
@@ -13,6 +14,7 @@ import Ratings from '@/components/Ratings.vue';
     <Hero />
     <FeatureSection />
     <Ratings />
+    <Awards />
     <Faq />
     <Cta />
     </main>
