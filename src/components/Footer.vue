96fade642aa7adf210e885e2c902ec869183d2ef
Sacha <124590090+usinedev@users.noreply.github.com>
2026-01-06 12:20:59 +0100

    footer - desktop

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,9 +3,75 @@
 </script>
 
 <template>
-    
+    <footer>
+        <div class="footerContainer">
+            <div class="top">
+                <div class="topLeft">
+                    <h2>Brilliant solutions for your ideas</h2>
+                    <h3>Modern looking websites.</h3>
+                </div>
+                <div class="topRight">
+                    <button class="docBtn">
+                        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M7.5 4.5C6.67157 4.5 6 5.17157 6 6V15.4013C6.44126 15.1461 6.95357 15 7.5 15H18V4.5H7.5ZM18 16.5H7.5C6.67157 16.5 6 17.1716 6 18C6 18.8284 6.67157 19.5 7.5 19.5H18V16.5ZM4.5 18L4.5 6C4.5 4.34315 5.84315 3 7.5 3H18.75L19.5 3.75V21H7.5C5.84315 21 4.5 19.6569 4.5 18Z" fill="#1e90ff"/></svg>
+                        <span>Documentation</span>
+                    </button>
+                    <button class="purchaseBtn">
+                        Purchase now
+                    </button>
+                </div>
+            </div>
+            <div class="separator"></div>
+            <div class="mid">
+                <div class="midLeft">
+                    <a href=""><img src="../assets/logos/odoo-white.svg" alt="Logo de Odoo"></a>
+                    <p>Odoo attempts to bring the best development experience to designers and developers by offering the tools needed for having a quick and solid start in most web projects.</p>
+                    <div class="logos">
+                        <a href="">
+                            <img src="../assets/logos/dribbble.svg" alt="Logo de dribbble">
+                        </a>
+                        <a href="">
+                            <img src="../assets/logos/github.svg" alt="Logo de Github">
+                        </a>
+                        <a href="">
+                            <img src="../assets/logos/instagram.svg" alt="Logo de Instagram">
+                        </a>
+                        <a href="">
+                            <img src="../assets/logos/facebook.svg" alt="Logo de Facebook">
+                        </a>
+                    </div>
+                </div>
+                <div class="midRight">
+                    <div class="column">
+                        <p class="title">Account</p>
+                        <a href="">Profile</a>
+                        <a href="">Settings</a>
+                        <a href="">Billing</a>
+                        <a href="">Notifications</a>
+                    </div>
+                    <div class="column">
+                        <p class="title">About</p>
+                        <a href="">Services</a>
+                        <a href="">Pricing</a>
+                        <a href="">Contact</a>
+                        <a href="">Careers</a>
+                    </div>
+                    <div class="column">
+                        <p class="title">Company</p>
+                        <a href="">Community</a>
+                        <a href="">Help center</a>
+                        <a href="">Support</a>
+                    </div>
+                </div>
+            </div>
+            <div class="separator"></div>
+            <div class="bottom">
+                <p>© <span>Odoo</span>. All rights reserved</p>
+                <div class="links">
+                    <a href="">Terms</a>
+                    <a href="">Privacy</a>
+                    <a href="">Cookies</a>
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
-
-<style scoped lang="scss">
-
-</style>
